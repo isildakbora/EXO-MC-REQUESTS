c0ebb0749a25a03b6732acceb1213f3a6a1e75d7
Bora Isildak <bora.isildak@cern.ch>
2018-03-24 22:33:59 +0300

    Update ttbarDMjets.py

--- ttbarDMjets.py
+++ ttbarDMjets.py
@@ -3,13 +3,6 @@ from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
 from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
 
-externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/nfs/dust/cms/user/agrohsje/workarea/cms/gridpacks/madpacks/v2.4.2/SUBGRIDPACK_*_tarball.tar.xz'),
-    nEvents = cms.untracked.uint32(0),
-    numberOfParameters = cms.uint32(1),
-    outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
-)
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
                          maxEventsToPrint = cms.untracked.int32(1),
