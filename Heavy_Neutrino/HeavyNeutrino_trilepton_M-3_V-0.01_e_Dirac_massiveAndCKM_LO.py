f9a4341133a8d4eabfeead08e45230a3559c400f
isildakbora <bora.isildak@cern.ch>
2019-03-15 12:17:25 +0300

    file paths corrected
    file paths corrected

--- Heavy_Neutrino/HeavyNeutrino_trilepton_M-3_V-0.01_e_Dirac_massiveAndCKM_LO.py
+++ Heavy_Neutrino/HeavyNeutrino_trilepton_M-3_V-0.01_e_Dirac_massiveAndCKM_LO.py
@@ -1,12 +1,3 @@
-import FWCore.ParameterSet.Config as cms
-
-externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.4.2/exo_heavyNeutrino/displaced_2017/v1/HeavyNeutrino_trilepton_M-3_V-0.01_e_Dirac_massiveAndCKM_LO_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz'),
-    nEvents = cms.untracked.uint32(5000),
-    numberOfParameters = cms.uint32(1),
-    outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
-)
 
 import FWCore.ParameterSet.Config as cms
 
--- Heavy_Neutrino/HeavyNeutrino_trilepton_M-8_V-0.00151327459504_e_pre2017_massiveAndCKM_LO_pre2017.py
+++ Heavy_Neutrino/HeavyNeutrino_trilepton_M-8_V-0.00151327459504_e_pre2017_massiveAndCKM_LO_pre2017.py
@@ -1,12 +1,3 @@
-import FWCore.ParameterSet.Config as cms
-
-externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.4.2/exo_heavyNeutrino/displaced_pre2017/v1/HeavyNeutrino_trilepton_M-8_V-0.00151327459504_e_pre2017_massiveAndCKM_LO_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz'),
-    nEvents = cms.untracked.uint32(1100),
-    numberOfParameters = cms.uint32(1),
-    outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
-)
 
 import FWCore.ParameterSet.Config as cms
 
