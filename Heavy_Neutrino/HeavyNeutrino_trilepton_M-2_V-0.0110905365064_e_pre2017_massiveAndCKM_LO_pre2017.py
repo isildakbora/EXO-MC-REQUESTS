17c40e79a556891c89b30ae26b784f9ddec005d0
isildakbora <bora.isildak@cern.ch>
2019-03-30 18:30:01 +0300

    missing fragments are added
    missing fragments are added

--- Heavy_Neutrino/HeavyNeutrino_trilepton_M-2_V-0.0110905365064_e_pre2017_massiveAndCKM_LO_pre2017.py
+++ Heavy_Neutrino/HeavyNeutrino_trilepton_M-2_V-0.0110905365064_e_pre2017_massiveAndCKM_LO_pre2017.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.4.2/exo_heavyNeutrino/displaced_2017/v1/HeavyNeutrino_trilepton_M-4_V-0.00411096095822_e_Dirac_massiveAndCKM_LO_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz'),
-    nEvents = cms.untracked.uint32(5000),
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.4.2/exo_heavyNeutrino/displaced_pre2017/v1/HeavyNeutrino_trilepton_M-2_V-0.0110905365064_e_pre2017_massiveAndCKM_LO_slc6_amd64_gcc481_CMSSW_7_1_30_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(1100),
     numberOfParameters = cms.uint32(1),
     outputFile = cms.string('cmsgrid_final.lhe'),
     scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
@@ -11,7 +11,7 @@ externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
-from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
     maxEventsToPrint = cms.untracked.int32(1),
@@ -21,9 +21,9 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
     comEnergy = cms.double(13000.),
     PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
-        pythia8CP5SettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
         parameterSets = cms.vstring('pythia8CommonSettings',
-                                    'pythia8CP5Settings',
+                                    'pythia8CUEP8M1Settings',
                                     )
     )
 )
