25962637b2d9011628bb57aa52c4236311c61735
isildakbora <bora.isildak@cern.ch>
2019-04-01 21:51:50 +0300

    ps weights added
    ps weights added

--- Disappearing_Tracks/AMSB_chargino_M-200GeV_CTau-100cm_TuneCP5_13TeV_pythia8_cff.py
+++ Disappearing_Tracks/AMSB_chargino_M-200GeV_CTau-100cm_TuneCP5_13TeV_pythia8_cff.py
@@ -167,6 +167,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8GeneratorFilter",
     pythiaPylistVerbosity = cms.untracked.int32(0),
@@ -190,6 +191,7 @@ generator = cms.EDFilter("Pythia8GeneratorFilter",
        ),
         parameterSets = cms.vstring(
             'pythia8CommonSettings',
+            'pythia8PSweightsSettings',
             'pythia8CP5Settings',
             'processParameters')
     ),
